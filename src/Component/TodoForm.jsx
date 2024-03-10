f37d726603ffccbb905ac3c4a73aff8d16c90761
YASSER JERRARI <yasserjerrari2001@gmail.com>
2024-03-10 11:37:02 +0000

    adding the form

---
+++ src/Component/TodoForm.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import {FormControl,Container,TextField,Button} from '@mui/material'
+
+const TodoForm =()=>{
+    return (
+        <Container maxWidth="sm">
+            <form>
+                <FormControl fullWidth={true}> 
+                    <TextField label="je vais faire..." required={true} />
+                    <Button variant="contained" color="primary" type="submit">Ajouter Task</Button>
+                </FormControl>
+            </form>
+
+        </Container>
+
+    )
+}
+
+export default TodoForm;
